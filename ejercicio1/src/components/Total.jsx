d81bcbc0e6372d3535a4620b9b10239e051986eb
Carla Rodríguez <crodriguez42@ucol.mx>
2022-06-30 15:48:59 -0500

    actualizaciones

---
+++ ejercicio1/src/components/Total.jsx
@@ -0,0 +1,11 @@
+const Total = ({ courseInfo }) => {
+  let total = 0;
+
+  courseInfo.map((info) => {
+    total += info.exercises;
+  });
+
+  return <p>Number of Exercises: {total}</p>;
+};
+
+export default Total;
--- ejercicio1/src/main.jsx
+++ ejercicio1/src/main.jsx
@@ -1,54 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
 
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  );
-};
+// const Header = ({ course }) => {
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//     </div>
+//   );
+// };
 
-const Content = ({ parts, exercises }) => {
-  return (
-    <div>
-      {parts.map((part) => {
-        return <p>{part}</p>;
-      })}
-      <ul>
-        {exercises.map((exercise) => {
-          return <li>{exercise}</li>;
-        })}
-      </ul>
-    </div>
-  );
-};
+// const Content = ({ parts, exercises }) => {
+//   return (
+//     <div>
+//       {parts.map((part) => {
+//         return <p>{part}</p>;
+//       })}
+//       <ul>
+//         {exercises.map((exercise) => {
+//           return <li>{exercise}</li>;
+//         })}
+//       </ul>
+//     </div>
+//   );
+// };
 
-const Total = ({ totalExercises }) => {
-  return (
-    <div>
-      <p>{totalExercises}</p>
-    </div>
-  );
-};
+// const Total = ({ totalExercises }) => {
+//   return (
+//     <div>
+//       <p>{totalExercises}</p>
+//     </div>
+//   );
+// };
 
 const App = () => {
   const course = "Half Stack application development";
-  const parts = [
-    "Fundamentals of React",
-    "Using props to pass data",
-    "State of a component",
-  ];
-  const exercises = [10, 7, 14];
+  const part1 = "Fundamentals of React";
+  const part2 = "Passing data through props";
+  const part3 = "State of a component";
   const exercises1 = 10;
   const exercises2 = 7;
   const exercises3 = 14;
 
+  const courseInfo = [
+    { key: "fundamentals", part: "Fundamentals of React", exercises: 10 },
+    { key: "props", part: "Passing data through props", exercises: 7 },
+    { key: "state", part: "State of a component", exercises: 14 },
+    { key: "fetch", part: "Fetching resources from an API", exercises: 9 },
+  ];
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Total totalExercises={exercises1 + exercises2 + exercises3} />
+      <Header title={course} />
+      <Content courseInfo={courseInfo} />
+      <Total courseInfo={courseInfo} />
     </div>
   );
 };
